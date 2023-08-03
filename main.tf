3d9fa222e2dd27d97f116e1ee463004b6ab26611
clustermaster99 <marc@marclehner.com>
2023-08-03 18:21:09 -0500

    fmt

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ locals {
 ################################################################################
 
 resource "aws_vpc" "lehner-vpc" {
-    count = local.create_vpc ? 1 : 0
-    cidr_block          = var.cidr
-    tags = {
-    "Name" = var.name 
-    }
+  count      = local.create_vpc ? 1 : 0
+  cidr_block = var.cidr
+  tags = {
+    "Name" = var.name
+  }
 }
 
